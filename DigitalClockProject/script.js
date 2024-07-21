435e2917a692f70b4cde26e7539e27f672535a7a
Abdullah-MWT <abdullahdeveloper2018@gmail.com>
2024-07-21 14:42:05 +0500

    My Seventh JS Code Folder

---
+++ DigitalClockProject/script.js
@@ -0,0 +1,9 @@
+const clock = document.getElementById('clock');
+
+const date = new Date();
+clock.innerHTML = date.toLocaleTimeString();
+
+setInterval(function(){
+    const date = new Date();
+    clock.innerHTML = date.toLocaleTimeString(); 
+},1000)
